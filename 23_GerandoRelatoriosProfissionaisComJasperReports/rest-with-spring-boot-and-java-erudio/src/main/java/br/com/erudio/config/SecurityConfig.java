eb2025a5162d82ff2e00081f7d4b692d0984f9ed
joao_junio <joaojunio818@gmail.com>
2025-10-29 21:38:02 -0300

    Implementando AuthService

--- 23_GerandoRelatoriosProfissionaisComJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
+++ 23_GerandoRelatoriosProfissionaisComJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
@@ -33,16 +34,16 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
-            "", 8, 185000,
-            Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        PasswordEncoder bcryptEncoder = new BCryptPasswordEncoder();kim8
 
         Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("pbkdf2", pbkdf2Encoder);
-        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        encoders.put("bcrypt", bcryptEncoder);
 
-        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
-        return passwordEncoder();
+        DelegatingPasswordEncoder passwordEncoder =
+                new DelegatingPasswordEncoder("bcrypt", encoders);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(bcryptEncoder);
+        return passwordEncoder;
     }
 
     @Bean
